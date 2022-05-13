82f14f15abdc4b450efc0004bd9222a0f335b0ea
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-05-13 13:51:30 +0800

    修复栈上counter的可疑代码

--- tools/core/src/asset/config_asset.cpp
+++ tools/core/src/asset/config_asset.cpp
@@ -40,8 +40,8 @@ void* SJsonConfigImporter::Import(skr::io::RAMService* ioService, const SAssetRe
         return nullptr;
     }
 
-    ftl::AtomicFlag counter(&GetCookSystem()->GetScheduler());
-    counter.Set();
+    auto counter = SkrNew<ftl::AtomicFlag>(&GetCookSystem()->GetScheduler());
+    counter->Set();
     skr_ram_io_t ramIO = {};
     ramIO.bytes = nullptr;
     ramIO.offset = 0;
@@ -53,10 +53,15 @@ void* SJsonConfigImporter::Import(skr::io::RAMService* ioService, const SAssetRe
         auto pCounter = (ftl::AtomicFlag*)data;
         pCounter->Clear();
     };
-    ramIO.callback_datas[SKR_ASYNC_IO_STATUS_OK] = (void*)&counter;
+    ramIO.callback_datas[SKR_ASYNC_IO_STATUS_OK] = (void*)counter;
     skr_async_io_request_t ioRequest = {};
-    // ioService->request(record->project->vfs, &ramIO, &ioRequest);
-    // GetCookSystem()->scheduler.WaitForCounter(&counter, true);
+#if 1
+    ioService->request(record->project->vfs, &ramIO, &ioRequest);
+    GetCookSystem()->scheduler->WaitForCounter(counter, true);
+    SkrDelete(counter);
+    auto jsonString = simdjson::padded_string(ioRequest.bytes, ioRequest.size);
+    sakura_free(ioRequest.bytes);
+#else
     auto file = skr_vfs_fopen(record->project->vfs, u8Path.c_str(), SKR_FM_READ, SKR_FILE_CREATION_OPEN_EXISTING);
     SKR_DEFER({ skr_vfs_fclose(file); });
     auto size = skr_vfs_fsize(file);
@@ -65,8 +70,7 @@ void* SJsonConfigImporter::Import(skr::io::RAMService* ioService, const SAssetRe
     buffer[size] = 0;
     auto jsonString = simdjson::padded_string(buffer, size);
     sakura_free(buffer);
-    // auto jsonString = simdjson::padded_string(ioRequest.bytes, ioRequest.size);
-    // sakura_free(ioRequest.bytes);
+#endif
     simdjson::ondemand::parser parser;
     auto doc = parser.iterate(jsonString);
     skr_config_resource_t* resource = skr::resource::SConfigFactory::NewConfig(configType);
